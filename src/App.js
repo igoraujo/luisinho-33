22ef96594595876ba5bf02d23f7ca10fae343425
Igor Araújo <igor.araujo@squadra.com.br>
2020-09-30 10:27:16 -0300

    feat: teste deploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          TESTE <code>src/App.js</code> IGOR.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          TESTE
         </a>
       </header>
     </div>
